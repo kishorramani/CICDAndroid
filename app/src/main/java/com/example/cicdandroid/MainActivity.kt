94783b3925e510a71c8ac5bd4361edabca552717
KishorRamani <kishorkeepsmiling@gmail.com>
2024-07-08 22:19:17 +0530

    Introduced Pull Request check

--- app/src/main/java/com/example/cicdandroid/MainActivity.kt
+++ app/src/main/java/com/example/cicdandroid/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CICDAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "This is a gh-actions branch",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
